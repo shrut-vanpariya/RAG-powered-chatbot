c548768939117f8b72ec66fda7f3534bc4bcc022
Shrut Vanpariya <shrutvanpariya@gmail.com>
2024-06-13 23:13:02 +0530

    updates

--- app.py
+++ app.py
@@ -42,17 +42,16 @@ if not os.path.exists('vectorDB'):
 
 
 
-if 'template' not in st.session_state:
-   st.session_state.template = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+# if 'template' not in st.session_state:
+#    st.session_state.template = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
 
-   ---
-   Context: {context}
-   ---
-   History: {history}
+#    ---
+#    Context: {context}
+#    ---
+#    History: {history}
 
-
-   User: {question}
-   Chatbot:"""
+#    User: {question}
+#    Chatbot:"""
 
 
 # if 'template' not in st.session_state:
@@ -65,6 +64,17 @@ if 'template' not in st.session_state:
 
 #    User: {question}
 #    Chatbot:"""
+
+
+if 'template' not in st.session_state:
+   st.session_state.template = """You are AI Chatbot assistent for car insurance company Churchill. Reply based on the context given related to user query. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+   ---
+   Context: {context}
+   ---
+   History: {history}
+
+   User: {question}
+   Chatbot:"""
 
 
 if 'prompt' not in st.session_state:
@@ -85,12 +95,12 @@ if 'memory' not in st.session_state:
 if 'vectorstore' not in st.session_state:
    st.session_state.vectorstore = Chroma(persist_directory='vectorDB',
                                            embedding_function=OllamaEmbeddings(base_url='http://localhost:11434',
-                                           model="nomic-embed-text")
+                                           model="mxbai-embed-large")
                                            )
   
 if 'llm' not in st.session_state:
    st.session_state.llm = Ollama(base_url="http://localhost:11434",
-                                 model="llama2",
+                                 model="llama3",
                                  verbose=True,
                                  callback_manager=CallbackManager(
                                      [StreamingStdOutCallbackHandler()]),
@@ -127,7 +137,7 @@ if uploaded_file is not None:
 
            text_splitter = RecursiveCharacterTextSplitter(
                chunk_size=1500,
-               chunk_overlap=200,
+               chunk_overlap=150,
                length_function=len
            )
 
@@ -147,8 +157,11 @@ if uploaded_file is not None:
 #    print(st.session_state.vectorstore.get(include=[]))
 #    print(st.session_state.vectorstore.similarity_search_with_score('what is transformer', k=1))
    print("VectorDB is loded.")
+   existing_items = st.session_state.vectorstore.get(include=[])  # IDs are always included by default
+   existing_ids = set(existing_items["ids"])
+   print(f"Number of existing documents in DB: {len(existing_ids)}")
 
-   st.session_state.retriever = st.session_state.vectorstore.as_retriever()
+   st.session_state.retriever = st.session_state.vectorstore.as_retriever(search_kwargs={"k": 5})
 
 
    if 'qa_chain' not in st.session_state:
